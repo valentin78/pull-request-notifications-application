17085507b6f20765811cfcbe53ed321f19c71c42
Serghei Paduret <serghei.paduret@monsato.md>
2023-12-08 16:52:58 -0500

    build comment tooltip out of comments

--- src/app/components/pull-request-view/pull-request.component.ts
+++ src/app/components/pull-request-view/pull-request.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {PullRequest} from '../../models/models';
+import {BitbucketComment, PullRequest} from '../../models/models';
 import {BitbucketMergeResultOutcome, PullRequestStatus} from '../../models/enums';
 
 @Component({
@@ -18,6 +18,7 @@ export class PullRequestComponent implements OnInit {
   hasConflicts!: boolean;
   status!: string;
   commentsCount!: number;
+  commentsTooltip?: string;
 
   constructor() {
   }
@@ -37,9 +38,44 @@ export class PullRequestComponent implements OnInit {
       || 'OPEN';
 
     this.commentsCount = this.pullRequest.properties.commentCount || 0;
+    this.commentsTooltip = this.buildTooltip();
   }
 
   navigateTo(url: string) {
     window.open(url);
+  }
+
+  onDisplayComments(_: MouseEvent) {
+    console.log('comments', this.pullRequest.comments);
+  }
+
+  private buildTooltip(): string {
+
+    let tooltip = `${this.commentsCount} comments`;
+
+    if (this.pullRequest.comments?.length) {
+      tooltip += '\n';
+      const comments = this.pullRequest.comments.map(c => c.comment!);
+      tooltip = this.iterateThroughComments(tooltip, comments, 1);
+    }
+
+    return tooltip;
+  }
+
+  private iterateThroughComments(tooltip: string, comments: BitbucketComment[], nestLevel: number): string {
+    const tabs = new Array(nestLevel).join('\t');
+    nestLevel++;
+
+    comments.forEach((value, index) => {
+      const cmt = value;
+      tooltip += `${tabs}#${index + 1} ${cmt?.author?.name ?? '<author>'}: ${cmt?.text?.trim()}\n`;
+
+      if (cmt?.comments?.length) {
+        tooltip = this.iterateThroughComments(tooltip, cmt.comments, nestLevel);
+      }
+    });
+
+    tooltip += '\n';
+    return tooltip;
   }
 }
